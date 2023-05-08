29fc334a4eae03947f4bf566dd88f84d657197e3
Bernardo Henrique Gaya <7628889@univali.br>
2023-05-08 16:02:05 -0300

    Resolucao exercicios 1,2 e 3

---
+++ introducao/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introducao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExerciciosAula01 exercicio = new ExerciciosAula01();
+            exercicio.Exercicio03();
+        }
+    }
+}
